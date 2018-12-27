3c4d3800a8f29a2eab1f66552a65d9f76e7ce272
mine <headbang92@hotmail.com>
2018-12-27 09:02:05 +0300

    Create RecyclerViewAdapter.java

---
+++ app/src/main/java/com/androidtraining/imdbapp/RecyclerViewAdapter.java
@@ -0,0 +1,13 @@
+package com.androidtraining.imdbapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Mine Kandil on 27.12.2018.
+ */
+public class RecyclerViewAdapter {
+
+
+
+}
